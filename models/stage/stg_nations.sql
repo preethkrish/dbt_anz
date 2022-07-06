1f4628325132214a087cd0450735db1665755173
Preethi Krishnan <preethkrish@gmail.com>
2022-07-06 11:24:27 +0000

    changes

--- models/stage/stg_nations.sql
+++ models/stage/stg_nations.sql
@@ -1,1 +1,9 @@
-select * from sourcedb.nations
+with nation as (
+select
+        n_nationkey as nation_id,
+        n_regionkey as region_id,
+        n_name as name,
+        n_comment as comment
+from {{ source('src', 'nations') }}
+)
+select * from nation
