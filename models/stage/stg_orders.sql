586040591ccd606aeba6c22e27480de63769af3e
Preethi Krishnan <preethkrish@gmail.com>
2022-07-07 11:42:42 +0000

    Day 2 training

---
+++ models/stage/stg_orders.sql
@@ -0,0 +1,17 @@
+select
+        -- ids
+        o_orderkey as order_id,
+        o_custkey as customer_id,
+        
+        -- descriptions
+        o_comment as comment,
+        o_clerk as clerk_name,
+        -- numbers
+        o_totalprice as total_price,
+        -- statuses
+        o_orderstatus as status_code,
+        o_orderpriority as priority_code,
+        o_shippriority as ship_priority,
+        -- dates
+        o_orderdate as order_date
+    from {{ source('src', 'orders') }}
